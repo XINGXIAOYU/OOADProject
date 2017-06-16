5eb302f75d4bc6a7f8c081d62289e445167663e1
XINGXIAOYU <14302010053@fudan.edu.cn>
2017-06-16 21:54:50 +0800

    main test

--- src/test/java/ooad/SpringAppTests.java
+++ src/test/java/ooad/SpringAppTests.java
@@ -1,21 +1,111 @@
 package ooad;
 
-import junit.framework.Assert;
+import ooad.bean.Assignment;
+import ooad.bean.Company;
+import ooad.bean.Module;
+import ooad.bean.ModuleProcess;
+import ooad.common.Role;
+import ooad.common.exceptions.AuthorityException;
+import ooad.common.exceptions.NoSuchEntryException;
+import ooad.service.impl.AssignmentService;
+import ooad.service.impl.CompanyService;
+import ooad.service.impl.ModuleService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.List;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:spring-config.xml")
 public class SpringAppTests {
     @Autowired
-    private HelloService helloService;
+    private AssignmentService assignmentService;
+    private CompanyService companyService;
+    private ModuleService moduleService;
 
     @Test
-    public void testSayHello() {
-        Assert.assertEquals("Hello world!", helloService.sayHello());
+    public void mainTest() {
+
+        Role admin = Role.Admin;
+        //企业定义检查项目
+        try {
+            assert assignmentService.newAssignment(admin, "main test assignment", "for main test");
+            List<Assignment> assignmentList = assignmentService.getAssignments();
+            assert assignmentList.size() > 0;
+            assert assignmentList.get(assignmentList.size() - 1).getName().equals("main test assignment");
+            assert assignmentList.get(assignmentList.size() - 1).getContent().equals("for main test");
+        } catch (AuthorityException e) {
+            e.printStackTrace();
+        }
+
+        //企业设置模板
+        try {
+            assert moduleService.newModule(admin, "main test module", "for main test");
+            List<Module> moduleList = moduleService.getModules();
+            assert moduleList.size() > 0;
+            Module module = moduleList.get(moduleList.size() - 1);
+            assert module.getName().equals("main test module");
+            assert module.getDescription().equals("for main test");
+            List<Assignment> assignmentList = assignmentService.getAssignments();
+            if (assignmentList.size() > 0) {
+                Assignment assignment = assignmentList.get(assignmentList.size() - 1);
+                assert moduleService.addAssignmentToModule(admin, module.getId(), assignment.getId());
+                List<Assignment> module_assignmentList = moduleService.getModuleAssignments(module.getId());
+                Assignment module_assignment = module_assignmentList.get(module_assignmentList.size() - 1);
+                assert module_assignment.getName().equals("main test assignment");
+                assert module_assignment.getContent().equals("for main test");
+            }
+        } catch (AuthorityException e) {
+            e.printStackTrace();
+        } catch (NoSuchEntryException e) {
+            e.printStackTrace();
+        }
+
+        //企业发布模板给企业
+        List<Module> moduleList = moduleService.getModules();
+        if (moduleList.size() > 0) {
+            Module module = moduleList.get(moduleList.size() - 1);
+            assert module.getName().equals("main test module");
+            assert module.getDescription().equals("for main test");
+            List<Company> companyList = companyService.getCompanys();
+            if (companyList.size() > 0) {
+                Company company = companyList.get(0);
+                try {
+                    assert module.getModuleStatus().equals(Module.UNPUBLISHED);
+                    assert moduleService.publishModule(admin, module.getId(), company.getId(), "2016-07-01", "2017-07-01");
+                    assert module.getModuleStatus().equals(Module.PUBLISHED);
+                } catch (AuthorityException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        //企业完成模板
+        Role company = Role.Company;
+        int company_id = 1;
+        try {
+            List<Module> company_moduleList = companyService.getCModuleList(company, company_id);
+            if(company_moduleList.size()>0){
+                Module company_module = company_moduleList.get(0);
+                assert company_module.getName().equals("main test module");
+                assert company_module.getDescription().equals("for main test");
+                ModuleProcess moduleProcess = companyService.getModuleProcess(company,company_module.getId());
+                assert moduleProcess.getModule_id()==company_module.getId();
+                assert moduleProcess.getCompany_id()==company_id;
+                assert moduleProcess.getStatus().equals(ModuleProcess.UNCOMPLETED);
+                assert companyService.completeStatus(company,moduleProcess.getId());
+                ModuleProcess moduleProcess_after = companyService.getModuleProcess(company,company_module.getId());
+                assert moduleProcess_after.getStatus().equals(ModuleProcess.COMPLETED);
+            }
+        } catch (AuthorityException e) {
+            e.printStackTrace();
+        } catch (NoSuchEntryException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
--- src/test/java/ooad/service/ModuleServiceTest.java
+++ src/test/java/ooad/service/ModuleServiceTest.java
@@ -80,20 +80,25 @@ public class ModuleServiceTest {
     @Test
     public void testModifyModule() throws Exception {
         List<Module> modules = moduleService.findModule("new module");
-        Module module = modules.get(0);
-        moduleService.modifyModule(Role.Admin, module.getId(), "new module", "modified_admin");
-        List<Module> modules2 = moduleService.findModule("new module");
-        assert modules2.size() > 0;
-        assert modules2.get(0).getId() == module.getId();
-        assert modules2.get(0).getName().equals("new module");
-        assert modules2.get(0).getDescription().equals("modified_admin");
+        if (modules.size() > 0) {
+            Module module = modules.get(0);
+            moduleService.modifyModule(Role.Admin, module.getId(), "new module", "modified_admin");
+            List<Module> modules2 = moduleService.findModule("new module");
+            assert modules2.size() > 0;
+            assert modules2.get(0).getId() == module.getId();
+            assert modules2.get(0).getName().equals("new module");
+            assert modules2.get(0).getDescription().equals("modified_admin");
+        }
     }
 
     @Test(expected = AuthorityException.class)
     public void testModifyModuleAuthority() throws Exception {
         List<Module> modules = moduleService.findModule("new module");
-        Module module = modules.get(0);
-        moduleService.modifyModule(Role.Company, module.getId(), "new module", "modified_company");
+        if (modules.size() > 0) {
+            Module module = modules.get(0);
+            moduleService.modifyModule(Role.Company, module.getId(), "new module", "modified_company");
+        }
+
     }
 
     @Test
@@ -116,86 +121,101 @@ public class ModuleServiceTest {
     @Test
     public void testAddAssignmentToModule() throws Exception {
         List<Assignment> assignments = assignmentService.getAssignments();
-        Assignment assignment = assignments.get(assignments.size() - 1);
-        List<Module> modules = moduleService.getModules();
-        Module module = modules.get(0);
-        moduleService.addAssignmentToModule(Role.Admin, module.getId(), assignment.getId());
-        List<Assignment> module_assignments = moduleService.getModuleAssignments(module.getId());
-        Assignment assignment1 = module_assignments.get(module_assignments.size() - 1);
-        assert assignment1.getId() == assignment.getId();
-        assert assignment1.getName().equals(assignment.getName());
-        assert assignment1.getContent().equals(assignment.getContent());
+        if (assignments.size() > 0) {
+            Assignment assignment = assignments.get(assignments.size() - 1);
+            List<Module> modules = moduleService.getModules();
+            if (modules.size() > 0) {
+                Module module = modules.get(0);
+                moduleService.addAssignmentToModule(Role.Admin, module.getId(), assignment.getId());
+                List<Assignment> module_assignments = moduleService.getModuleAssignments(module.getId());
+                Assignment assignment1 = module_assignments.get(module_assignments.size() - 1);
+                assert assignment1.getId() == assignment.getId();
+                assert assignment1.getName().equals(assignment.getName());
+                assert assignment1.getContent().equals(assignment.getContent());
+            }
+        }
     }
 
     @Test(expected = AuthorityException.class)
     public void testAddAssignmentToModuleAuthority() throws Exception {
         List<Assignment> assignments = assignmentService.getAssignments();
-        Assignment assignment = assignments.get(assignments.size() - 1);
-        List<Module> modules = moduleService.getModules();
-        Module module = modules.get(0);
-        moduleService.addAssignmentToModule(Role.Company, module.getId(), assignment.getId());
+        if (assignments.size() > 0) {
+            Assignment assignment = assignments.get(assignments.size() - 1);
+            List<Module> modules = moduleService.getModules();
+            if (modules.size() > 0) {
+                Module module = modules.get(0);
+                moduleService.addAssignmentToModule(Role.Company, module.getId(), assignment.getId());
+            }
+        }
     }
 
     @Test
     public void testDeleteAssignmentFromModule() throws Exception {
         List<Module> modules = moduleService.getModules();
-        Module module = modules.get(0);
-        List<Assignment> assignments = moduleService.getModuleAssignments(module.getId());
-        if (assignments.size() > 0) {
-            Assignment assignment = assignments.get(assignments.size() - 1);
-            moduleService.deleteAssignmentFromModule(Role.Admin, module.getId(), assignment.getId());
-
-            List<Assignment> assignments2 = moduleService.getModuleAssignments(module.getId());
-            boolean hasDeletedAssignment = true;
-            for (Assignment i : assignments2) {
-                if (i.getId() == assignment.getId()) {
-                    hasDeletedAssignment = false;
+        if (modules.size() > 0) {
+            Module module = modules.get(0);
+            List<Assignment> assignments = moduleService.getModuleAssignments(module.getId());
+            if (assignments.size() > 0) {
+                Assignment assignment = assignments.get(assignments.size() - 1);
+                moduleService.deleteAssignmentFromModule(Role.Admin, module.getId(), assignment.getId());
+                List<Assignment> assignments2 = moduleService.getModuleAssignments(module.getId());
+                boolean hasDeletedAssignment = true;
+                for (Assignment i : assignments2) {
+                    if (i.getId() == assignment.getId()) {
+                        hasDeletedAssignment = false;
+                    }
                 }
+                assert hasDeletedAssignment == true;
             }
-            assert hasDeletedAssignment == true;
         }
     }
 
     @Test(expected = AuthorityException.class)
     public void testDeleteAssignmentFromModuleAuthority() throws Exception {
         List<Module> modules = moduleService.getModules();
-        Module module = modules.get(0);
-        List<Assignment> assignments = moduleService.getModuleAssignments(module.getId());
-        if (assignments.size() > 0) {
-            Assignment assignment = assignments.get(assignments.size() - 1);
-            moduleService.deleteAssignmentFromModule(Role.Company, module.getId(), assignment.getId());
+        if (modules.size() > 0) {
+            Module module = modules.get(0);
+            List<Assignment> assignments = moduleService.getModuleAssignments(module.getId());
+            if (assignments.size() > 0) {
+                Assignment assignment = assignments.get(assignments.size() - 1);
+                moduleService.deleteAssignmentFromModule(Role.Company, module.getId(), assignment.getId());
+            }
         }
     }
 
     @Test(expected = NoSuchEntryException.class)
     public void testAddAssignmentToModuleNoSuchEntry() throws Exception {
         List<Module> modules = moduleService.getModules();
-        Module module = modules.get(0);
-        moduleService.deleteAssignmentFromModule(Role.Admin, module.getId(), -1);
+        if (modules.size() > 0) {
+            Module module = modules.get(0);
+            moduleService.deleteAssignmentFromModule(Role.Admin, module.getId(), -1);
+        }
     }
 
 
     @Test
     public void testPublishModule() throws Exception {
         List<Module> modules = moduleService.getModules();
-        Module module = modules.get(0);
-        assert module.getModuleStatus().equals(Module.UNPUBLISHED);
-        List<Company> companies = companyService.getCompanys();
-        Company company = companies.get(0);
-        String startDate = "2017-07-01";
-        String finishDate = "2017-08-01";
-        assert moduleService.publishModule(Role.Admin, module.getId(), company.getId(), startDate, finishDate) == true;
-        modules = moduleService.getModules();
-        module = modules.get(0);
-        assert module.getModuleStatus().equals(Module.PUBLISHED);
-        List<Company> module_companys = moduleService.getModuleCompanys(module.getId());
-        boolean hasPublished = false;
-        for (Company c : module_companys) {
-            if (c.getId() == company.getId()) {
-                hasPublished = true;
+        if(modules.size()>0) {
+            Module module = modules.get(0);
+            assert module.getModuleStatus().equals(Module.UNPUBLISHED);
+            List<Company> companies = companyService.getCompanys();
+            Company company = companies.get(0);
+            String startDate = "2017-07-01";
+            String finishDate = "2017-08-01";
+            assert moduleService.publishModule(Role.Admin, module.getId(), company.getId(), startDate, finishDate);
+            modules = moduleService.getModules();
+            module = modules.get(0);
+            assert module.getModuleStatus().equals(Module.PUBLISHED);
+            List<Company> module_companys = moduleService.getModuleCompanys(module.getId());
+            boolean hasPublished = false;
+            for (Company c : module_companys) {
+                if (c.getId() == company.getId()) {
+                    hasPublished = true;
+                }
             }
+            assert hasPublished == true;
         }
-        assert hasPublished == true;
     }
 
     @Test(expected = AuthorityException.class)
@@ -208,10 +228,12 @@ public class ModuleServiceTest {
     @Test
     public void testGetModuleCompanys() throws Exception {
         List<Module> modules = moduleService.getModules();
-        Module module = modules.get(0);
-        if (module.getModuleStatus().equals(Module.PUBLISHED)) {
-            List<Company> module_companys = moduleService.getModuleCompanys(module.getId());
-            assert module_companys.size() > 0;
+        if(modules.size()>0) {
+            Module module = modules.get(0);
+            if (module.getModuleStatus().equals(Module.PUBLISHED)) {
+                List<Company> module_companys = moduleService.getModuleCompanys(module.getId());
+                assert module_companys.size() > 0;
+            }
         }
     }
 
@@ -223,12 +245,14 @@ public class ModuleServiceTest {
     @Test
     public void testGetModuleProcesses() throws Exception {
         List<Module> modules = moduleService.getModules();
-        Module module = modules.get(0);
-        List<ModuleProcess> moduleProcesses = moduleService.getModuleProcesses(module.getId());
-        if(moduleProcesses.size()>0){
-            assert moduleProcesses.get(0).getId()==1;
-            assert moduleProcesses.get(0).getCompany_id()==1;
-            assert moduleProcesses.get(0).getModule_id()==1;
+        if(modules.size()>0) {
+            Module module = modules.get(0);
+            List<ModuleProcess> moduleProcesses = moduleService.getModuleProcesses(module.getId());
+            if (moduleProcesses.size() > 0) {
+                assert moduleProcesses.get(0).getId() == 1;
+                assert moduleProcesses.get(0).getCompany_id() == 1;
+                assert moduleProcesses.get(0).getModule_id() == 1;
+            }
         }
     }
 
